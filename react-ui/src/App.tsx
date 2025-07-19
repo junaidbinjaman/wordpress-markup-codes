32a4ee76b6856ed2b583b5660f0ed33c112a8492
junaidbinjaman <me@junaidbinjaman.com>
2025-07-19 23:56:47 +0600

    feat: The form to create the post is ready. Note: It's time to fetch the api and publish the post.

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,11 +1,10 @@
-import {Button} from "@/components/ui/button.tsx";
+import Form from "@/components/Form.tsx";
 
 function App() {
 
   return (
     <>
-      <h1 className='text-5xl text-pink-500 font-bold border-2 border-slate-400 rounded py-3 px-5'>Hello and Welcome..</h1>
-        <Button className='mt-5 cursor-pointer'>Click Me</Button>
+        <Form title={'Create a New Post'} />
     </>
   )
 }
---
+++ react-ui/src/components/Form.tsx
@@ -0,0 +1,73 @@
+import {Button} from "@/components/ui/button.tsx";
+import {Input} from "@/components/ui/input.tsx";
+import {Textarea} from "@/components/ui/textarea.tsx";
+import {useForm} from "react-hook-form";
+import type {SubmitHandler} from "react-hook-form";
+import {useEffect, useState} from "react";
+
+type FormProps = {
+    title: string;
+}
+
+type InputTypes = {
+    postTitle: string;
+    postContent: string;
+}
+
+function Form({title}: FormProps) {
+    const [postData, setPostData] = useState({
+        postTitle: '',
+        postContent: ''
+    });
+
+    const {register, handleSubmit, watch, formState: {errors}} = useForm<InputTypes>({
+        defaultValues: {
+            postTitle: postData.postTitle,
+            postContent: postData.postContent
+        }
+    });
+
+    const onSubmit: SubmitHandler<InputTypes> = (data: InputTypes) => {
+        console.log(data);
+        setPostData({postTitle: '', postContent: ''});
+    }
+
+    useEffect(() => {
+        setPostData({postTitle: watch('postTitle'), postContent: watch('postContent')});
+    }, [watch('postTitle'), watch('postContent')]);
+
+
+    return (
+        <>
+            <h3 className='!text-3xl text-slate-800 border-slate-400 rounded py-3'>{title}</h3>
+            <form onSubmit={handleSubmit(onSubmit)} className='shadow p-6 rounded mt-5'>
+                <div className='mb-5'>
+                    <label htmlFor='post-title' className='!text-[18px]'>Post Title</label>
+                    <Input
+                        {...register('postTitle', {required: 'Tis field is required'})}
+                        value={postData.postTitle}
+                        id='post-title'
+                        placeholder='Post title'
+                        type='text'
+                    />
+                    <p className='text-[12px] text-red-600 italic'>{errors.postTitle?.message}</p>
+                </div>
+                <div>
+                    <label htmlFor='post-content' className='!text-[18px]'>Post Content</label>
+                    <Textarea
+                        {...register('postContent', {required: 'Post content is required'})}
+                        value={postData.postContent}
+                        id='post-content'
+                        placeholder='Your post content..'
+                    >
+                    </Textarea>
+                    <p className='text-[12px] text-red-600 italic'>{errors.postContent?.message}</p>
+                </div>
+                <Button
+                    className='mt-5 cursor-pointer text-lg px-10 py-6 active:scale-75 bg-slate-800 hover:bg-slate-700'>Publish</Button>
+            </form>
+        </>
+    )
+}
+
+export default Form
