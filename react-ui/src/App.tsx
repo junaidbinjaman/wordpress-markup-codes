e9eaf4d031011bdfabfe057e031ef16c39001d17
junaidbinjaman <me@junaidbinjaman.com>
2025-07-19 22:18:41 +0600

    patch: React-ts tailwindcss shadcn, We have all those libraries now on board. So we are ready to rock and roll on the frotend development.

---
+++ react-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl text-pink-500 font-bold border-2 border-slate-400 rounded py-3 px-5'>Hello and Welcome..</h1>
+        <Button className='mt-5 cursor-pointer'>Click Me</Button>
+    </>
+  )
+}
+
+export default App
