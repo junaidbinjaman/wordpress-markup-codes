34938c36b53decfdaa3e9705f20d113987f194f5
junaidbinjaman <me@junaidbinjaman.com>
2025-08-19 07:27:43 +0600

    feat: All the dummy and test pages are gone. So the project is now ready for a fresh start.

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,3 @@ Route::get('/services', function() {
 Route::get('/contact', function (){
     return view('pages.contact');
 })->name('contact');
-
-Route::get('/example', function () {
-    return view('pages.example');
-});
--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,9 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true })
+        return pages[`./Pages/${name}.tsx`]
+    },
+
 });
